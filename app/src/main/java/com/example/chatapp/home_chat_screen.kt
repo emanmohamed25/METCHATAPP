4995c4302ab139905564388adc0cd67107702053
abdelrahman1820 <124547898+abdelrahman1820@users.noreply.github.com>
2023-02-18 18:26:54 +0200

    first3pages

--- app/src/main/java/com/example/chatapp/home_chat_screen.kt
+++ app/src/main/java/com/example/chatapp/home_chat_screen.kt
@@ -3,9 +3,9 @@ package com.example.chatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class login22 : AppCompatActivity() {
+class home_chat_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login22)
+        setContentView(R.layout.activity_home_chat_screen)
     }
 }
