6208ffe37437b2842a778c328297d57a327d79e4
abdelrahman1820 <124547898+abdelrahman1820@users.noreply.github.com>
2023-04-01 16:40:12 +0200

    shared prefrance comit and student design

---
+++ app/src/main/java/com/example/chatapp/BubbleAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.chatapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class BubbleAdapter (
+    private val contex: Context,
+    private val BubbleItem: MutableList<bubble_chat_data>,
+    private val listener: (ChatData) -> Unit,
+) : RecyclerView.Adapter<BubbleAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=
+        BubbleAdapter.ViewHolder(LayoutInflater.from(contex).inflate(R.layout.container_receive_messages,parent,false))
+
+    override fun getItemCount(): Int = BubbleItem.size
+
+    override fun onBindViewHolder(holder: BubbleAdapter.ViewHolder, position: Int) {
+        val  data :bubble_chat_data=BubbleItem[position]
+        holder.mess.text= data.message
+        holder.timemess.text=data.timemessage
+
+    }
+    class ViewHolder(itemview: View ) :
+        RecyclerView.ViewHolder(itemview) {
+            val mess=itemview.findViewById<TextView>(R.id.bubble_message)
+            val timemess=itemview.findViewById<TextView>(R.id.bubble_time_message)
+
+
+        }
+
+
+
+}
---
+++ app/src/main/java/com/example/chatapp/bubble_chat_data.kt
@@ -0,0 +1,3 @@
+package com.example.chatapp
+
+data class bubble_chat_data(val message:String, val timemessage:String)
