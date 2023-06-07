db0d1b43ec864f8dfd533ceb8dc19db4e28f8f53
emanmohamed25 <69373353+emanmohamed25@users.noreply.github.com>
2023-06-07 21:39:57 +0200

    updates

--- app/src/main/java/com/example/chatapp/doctor/newchat/admin/util/Constants.kt
+++ app/src/main/java/com/example/chatapp/doctor/newchat/admin/util/Constants.kt
@@ -4,13 +4,13 @@ class Constants {
     companion object {
         //                const val BASE_URL = "http://192.168.43.43:80/chatapp/public/api/"
 
-        const val BASE_URL = "http://192.168.1.20:80/chatapp/public/api/"
-       // const val BASE_URL ="http://172.20.10.11/chatapp/public/api/"
+//        const val BASE_URL = "http://192.168.1.20:80/chatapp/public/api/"
+        // const val BASE_URL ="http://172.20.10.11/chatapp/public/api/"
 
 
-        //         const val BASE_URL = "http://192.168.1.201/chatapp/public/api/"
+        const val BASE_URL = "http://192.168.1.3:80/chatapp/public/api/"
         const val MY_SHARED = "shared_access_token"
         const val REQUEST_CODE_PICKER = 100
-        const val DEFAUKT_BUFFER_SIZE=1048
+        const val DEFAUKT_BUFFER_SIZE = 1048
     }
 }
